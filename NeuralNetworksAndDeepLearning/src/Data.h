59dbcca3e0c0f7c4ec57bf6f94e3b870d831e27b
jhkim <jhkim@localhost>
2016-08-29 18:51:21 +0900

    - Data, SyncMem에 주석 추가

--- NeuralNetworksAndDeepLearning/src/Data.h
+++ NeuralNetworksAndDeepLearning/src/Data.h
@@ -18,7 +18,10 @@ using namespace std;
 
 
 
-
+/**
+ * @brief Layer 특정 단계에서의 data, gradient를 pair로 warpping, util method를 제공하는 클래스
+ * @details Layer의 입력, 출력, Parameter에 해당하는 data와 gradient에 적용
+ */
 template <typename Dtype>
 class Data {
 public:
@@ -27,84 +30,248 @@ public:
 
 	void reshape(const vector<uint32_t>& shape);
 
+	/**
+	 * @details 데이터의 수정할 수 없는 호스트 메모리 포인터 조회한다.
+	 * @return 데이터의 수정할 수 없는 호스트 메모리 포인터
+	 */
 	const Dtype* host_data();
+	/**
+	 * @details 데이터의 수정할 수 없는 디바이스 메모리 포인터 조회한다.
+	 * @return 데이터의 수정할 수 없는 디바이스 메모리 포인터
+	 */
 	const Dtype* device_data();
+	/**
+	 * @details 그레디언트의 수정할 수 없는 호스트 메모리 포인터 조회한다.
+	 * @return 그레디언트의 수정할 수 없는 호스트 메모리 포인터
+	 */
 	const Dtype* host_grad();
+	/**
+	 * @details 그레디언트의 수정할 수 없는 디바이스 메모리 포인터 조회한다.
+	 * @return 그레디언트의 수정할 수 없는 디바이스 메모리 포인터
+	 */
 	const Dtype* device_grad();
 
+	/**
+	 * @details 데이터의 수정할 수 있는 호스트 메모리 포인터 조회한다.
+	 * @return 데이터의 수정할 수 있는 호스트 메모리 포인터
+	 */
 	Dtype* mutable_host_data();
+	/**
+	 * @details 데이터의 수정할 수 있는 디바이스 메모리 포인터 조회한다.
+	 * @return 데이터의 수정할 수 있는 디바이스 메모리 포인터
+	 */
 	Dtype* mutable_device_data();
+	/**
+	 * @details 그레디언트의 수정할 수 있는 호스트 메모리 포인터 조회한다.
+	 * @return 그레디언트의 수정할 수 있는 호스트 메모리 포인터
+	 */
 	Dtype* mutable_host_grad();
+	/**
+	 * @details 그레디언트의 수정할 수 있는 디바이스 메모리 포인터 조회한다.
+	 * @return 그레디언트의 수정할 수 있는 디바이스 메모리 포인터
+	 */
 	Dtype* mutable_device_grad();
 
-
-
-
+	/**
+	 * @details 데이터의 호스트 메모리에 _count만큼 값을 복사한다.
+	 * @param data 복사할 Data
+	 * @param offset data의 포인터에 대한 offset
+	 */
 	void set_host_data(Data* data, const uint32_t offset=0) { set_host_data(data->host_data()+offset); }
+	/**
+	 * @details 데이터의 디바이스 메모리에 _count만큼 값을 복사한다.
+	 * @param data 복사할 Data
+	 * @param offset data의 포인터에 대한 offset
+	 */
 	void set_device_data(Data* data, const uint32_t offset=0) { set_device_data(data->device_data()+offset); }
+	/**
+	 * @details 그레디언트의 호스트 메모리에 _count만큼 값을 복사한다.
+	 * @param grad 복사할 Data
+	 * @param offset grad의 포인터에 대한 offset
+	 */
 	void set_host_grad(Data* grad, const uint32_t offset=0) { set_host_grad(grad->host_grad()+offset); }
+	/**
+	 * @details 그레디언트의 디바이스 메모리에 _count만큼 값을 복사한다.
+	 * @param grad 복사할 Data
+	 * @param offset grad의 포인터에 대한 offset
+	 */
 	void set_device_grad(Data* grad, const uint32_t offset=0) { set_device_grad(grad->device_grad()+offset); }
 
-	//void set_data(const Dtype* data, CopyType copyType);
+	/**
+	 * @details 데이터의 호스트 메모리에 _count만큼 값을 복사한다.
+	 * @param data 복사할 로우 데이터의 포인터
+	 */
 	void set_host_data(const Dtype* data);
+	/**
+	 * @details 데이터의 호스트 메모리에 _count만큼 주어진 디바이스 메모리 포인터로부터 값을 복사한다.
+	 * @param data 복사할 로우 디바이스 데이터의 포인터
+	 */
 	void set_host_with_device_data(const Dtype* data);
+	/**
+	 * @details 데이터의 디바이스 메모리에 _count만큼 주어진 호스트 메모리 포인터로부터 값을 복사한다.
+	 * @param data 복사할 로우 호스트 데이터의 포인터
+	 */
 	void set_device_with_host_data(const Dtype* data);
+	/**
+	 * @details 데이터의 디바이스 메모리에 _count만큼 값을 복사한다.
+	 * @param data 복사할 로우 디바이스 데이터의 포인터
+	 */
 	void set_device_data(const Dtype* data);
 
+
+	/**
+	 * @details 그레디언트의 호스트 메모리에 _count만큼 값을 복사한다.
+	 * @param data 복사할 로우 호스트 데이터의 포인터
+	 */
 	void set_host_grad(const Dtype* grad);
+	/**
+	 * @details 그레디언트의 디바이스 메모리에 _count만큼 값을 복사한다.
+	 * @param data 복사할 로우 디바이스 데이터의 포인터
+	 */
 	void set_device_grad(const Dtype* grad);
 
 
+	/**
+	 * @details 데이터의 호스트 메모리를 0으로 초기화한다.
+	 */
 	void reset_host_data();
+	/**
+	 * @details 데이터의 디바이스 메모리를 0으로 초기화한다.
+	 */
 	void reset_device_data();
+	/**
+	 * @details 그레디언트의 호스트 메모리를 0으로 초기화한다.
+	 */
 	void reset_host_grad();
+	/**
+	 * @details 그레디언트의 디바이스 메모리를 0으로 초기화한다.
+	 */
 	void reset_device_grad();
 
 
+	/**
+	 * @details 데이터의 호스트 메모리에 주어진 Data의 값을 더한다.
+	 * @param data 더 할 Data
+	 * @param offset data의 포인터에 대한 offset
+	 */
 	void add_host_data(Data* data, const uint32_t offset=0) { add_host_data(data->host_data()+offset); }
+	/**
+	 * @details 데이터의 디바이스 메모리에 주어진 Data의 값을 더한다.
+	 * @param data 더 할 Data
+	 * @param offset data의 포인터에 대한 offset
+	 */
 	void add_device_data(Data* data, const uint32_t offset=0) { add_device_data(data->device_data()+offset); }
+	/**
+	 * @details 그레디언트의 호스트 메모리에 주어진 Data의 값을 더한다.
+	 * @param grad 더 할 Data
+	 * @param offset data의 포인터에 대한 offset
+	 */
 	void add_host_grad(Data* grad, const uint32_t offset=0) { add_host_grad(grad->host_grad()+offset); }
+	/**
+	 * @details 그레디언트의 디바이스 메모리에 주어진 Data의 값을 더한다.
+	 * @param grad 더 할 Data
+	 * @param offset data의 포인터에 대한 offset
+	 */
 	void add_device_grad(Data* grad, const uint32_t offset=0) { add_device_grad(grad->device_grad()+offset); }
 
+	/**
+	 * @details 데이터의 호스트 메모리에 주어진 로우 호스트 포인터의 메모리 값을 더한다.
+	 * @param data 더 할 로우 호스트 메모리 포인터
+	 */
 	void add_host_data(const Dtype* data);
+	/**
+	 * @details 데이터의 디바이스 메모리에 주어진 로우 디바이스 포인터의 메모리 값을 더한다.
+	 * @param data 더 할 로우 디바이스 메모리 포인터
+	 */
 	void add_device_data(const Dtype* data);
+	/**
+	 * @details 그레디언트의 호스트 메모리에 주어진 로우 호스트 포인터의 메모리 값을 더한다.
+	 * @param grad 더 할 로우 호스트 메모리 포인터
+	 */
 	void add_host_grad(const Dtype* grad);
+	/**
+	 * @details 그레디언트의 디바이스 메모리에 주어진 로우 디바이스 포인터의 메모리 값을 더한다.
+	 * @param grad 더 할 로우 디바이스 메모리 포인터
+	 */
 	void add_device_grad(const Dtype* grad);
 
-
+	/**
+	 * @details 데이터의 호스트 메모리를 스케일링한다.
+	 * @param scale 스케일링할 스케일
+	 */
 	void scale_host_data(const float scale);
+	/**
+	 * @details 데이터의 디바이스 메모리를 스케일링한다.
+	 * @param scale 스케일링할 스케일
+	 */
 	void scale_device_data(const float scale);
+	/**
+	 * @details 그레디언트의 호스트 메모리를 스케일링한다.
+	 * @param scale 스케일링할 스케일
+	 */
 	void scale_host_grad(const float scale);
+	/**
+	 * @details 그레디언트의 디바이스 메모리를 스케일링한다.
+	 * @param scale 스케일링할 스케일
+	 */
 	void scale_device_grad(const float scale);
 
 
+	/**
+	 * @details 데이터의 디바이스 메모리의 제곱합을 구한다.
+	 * @param 데이터의 디바이스 메모리 제곱합
+	 */
 	double sumsq_device_data();
+	/**
+	 * @details 그레디언트의 디바이스 메모리의 제곱합을 구한다.
+	 * @param 그레디언트의 디바이스 메모리 제곱합
+	 */
 	double sumsq_device_grad();
 
 
+	/**
+	 * @details Data의 batch shape를 조회한다.
+	 * @return Data의 batch shape
+	 */
 	inline uint32_t batches() const { return _shape[0]; }
+	/**
+	 * @details Data의 channel shape를 조회한다.
+	 * @return Data의 channel shape
+	 */
 	inline uint32_t channels() const { return _shape[1]; }
+	/**
+	 * @details Data의 height shape를 조회한다.
+	 * @return Data의 height shape
+	 */
 	inline uint32_t height() const { return _shape[2]; }
+	/**
+	 * @details Data의 width shape를 조회한다.
+	 * @return Data의 width shape
+	 */
 	inline uint32_t width() const { return _shape[3]; }
 
-
+	/**
+	 * @details 데이터를 shape에 따라 화면에 출력한다.
+	 * @param head 출력할 때 헤드에 쓰일 문구
+	 */
 	void print_data(const string& head);
+	/**
+	 * @details 그레디언트를 shape에 따라 화면에 출력한다.
+	 * @param head 출력할 때 헤드에 쓰일 문구
+	 */
 	void print_grad(const string& head);
 
 
 
 private:
-	vector<uint32_t> _shape;
+	vector<uint32_t> _shape;			///< Data의 shape, Batches, Channels, Rows, Columns의 4차원 벡터로 구성
 
-	SyncMem<Dtype> _data;
-	SyncMem<Dtype> _grad;
+	SyncMem<Dtype> _data;				///< Data의 데이터
+	SyncMem<Dtype> _grad;				///< Data의 그레디언트
 
-	size_t _count;
+	size_t _count;						///< Data 메모리의 크기, 엘레먼트의 수 (Batches*Channels*Rows*Columns)
 
 	const static uint32_t SHAPE_SIZE = 4;
-
-
-
 
 public:
 	static uint32_t printConfig;
--- NeuralNetworksAndDeepLearning/src/SyncMem.h
+++ NeuralNetworksAndDeepLearning/src/SyncMem.h
@@ -13,75 +13,144 @@
 
 #include "Util.h"
 
+/**
+ * @brief 복사할 SRC와 DEST를 지정하는 열거형
+ */
 enum SyncMemCopyType {
-	HostToHost=0,
-	HostToDevice=1,
-	DeviceToHost=2,
-	DeviceToDevice=3
+	HostToHost=0,			// 호스트에서 호스트 복사
+	HostToDevice=1,			// 호스트에서 디바이스 복사
+	DeviceToHost=2,			// 디바이스에서 호스트 복사
+	DeviceToDevice=3		// 디바이스에서 디바이스 복사
 };
 
 
-
+/**
+ * @brief 동기화된 호스트, 디바이스 메모리 클래스
+ * @details 읽기만 수행할 경우 non_mutable getter를 통해 포인터를 조회할 것
+ *          mutable getter를 이용하는 경우 무조건 데이터에 변경이 있다고 보고
+ *          대응 메모리 조회시 복사가 일어나게 된다.
+ */
 template <typename Dtype>
 class SyncMem {
 public:
 	SyncMem();
 	virtual ~SyncMem();
 
+	/**
+	 * @details 지정된 사이즈로 호스트와 디바이스에 메모리를 할당, 초기화한다.
+	 * @param size 할당할 메모리 사이즈
+	 */
 	void reshape(size_t size);
 
+	/**
+	 * @details 읽기 전용의 호스트 메모리 포인터를 조회한다.
+	 * @return 읽기 전용의 호스트 메모리 포인터
+	 */
 	const Dtype* host_mem();
+	/**
+	 * @details 읽기 전용의 디바이스 메모리 포인터를 조회한다.
+	 * @return 읽기 전용의 디바이스 메모리 포인터
+	 */
 	const Dtype* device_mem();
 
+	/**
+	 * @details 읽기/쓰기 가능한 호스트 메모리 포인터를 조회한다.
+	 * @return 읽기/쓰기 가능한 호스트 메모리 포인터
+	 */
 	Dtype* mutable_host_mem();
+	/**
+	 * @details 읽기/쓰기 가능한 디바이스 메모리 포인터를 조회한다.
+	 * @return 읽기/쓰기 가능한 디바이스 메모리 포인터
+	 */
 	Dtype* mutable_device_mem();
 
-
+	/**
+	 * @details 주어진 데이터를 지정된 SRC에서 지정된 DEST로 복사한다.
+	 * @param mem 복사할 데이터 메모리 포인터
+	 * @param copyType 데이터를 복사할 SRC/DEST를 지정하는 열거형
+	 */
 	void set_mem(const Dtype* mem, SyncMemCopyType copyType);
 
+	/**
+	 * @details 호스트 메모리를 0으로 초기화한다.
+	 */
 	void reset_host_mem();
+	/**
+	 * @details 디바이스 메모리를 0으로 초기화한다.
+	 */
 	void reset_device_mem();
 
+	/**
+	 * @details 호스트 메모리에 주어진 호스트 포인터의 메모리값을 더한다.
+	 * @param mem 더 할 호스트 메모리의 포인터
+	 */
 	void add_host_mem(const Dtype* mem);
 
 	/**
-	 * @details float, double 타입의 template에 한해 사용
+	 * @details 디바이스 메모리에 주어진 디바이스 포인터의 메모리값을 더한다.
+	 * @param mem 더 할 디바이스 메모리의 포인터
 	 */
 	void add_device_mem(const Dtype* mem);
 
+	/**
+	 * @details 호스트 메모리의 값을 스케일링한다.
+	 * @param scale 스케일링할 스케일
+	 */
 	void scale_host_mem(const float scale);
 	/**
-	 * @details float, double 타입의 template에 한해 사용
+	 * @details 디바이스 메모리의 값을 스케일링한다.
+	 * @param scale 스케일링할 스케일
 	 */
 	void scale_device_mem(const float scale);
 
-	//Dtype sumsq_host_mem();
 	/**
-	 * @details float, double 타입의 template에 한해 사용
+	 * @details 디바이스 메모리의 값을 제곱합한다.
+	 * @return 디바이스 메모리 값의 제곱합
 	 */
 	double sumsq_device_mem();
 
-
+	/**
+	 * @details 메모리의 값을 출력한다.
+	 * @param head 출력할 때 헤드에 쓰일 문구
+	 */
 	void print(const string& head);
+	/**
+	 * @details 메모리의 값을 출력한다.
+	 * @param head 출력할 때 헤드에 쓰일 문구
+	 * @param shape 출력 포맷 shape (batches, columns, rows, columns)
+	 */
 	void print(const string& head, const std::vector<uint32_t>& shape);
 
 
 private:
+	/**
+	 * @details 디바이스 메모리의 변경 여부를 확인하고
+	 *          변경된 경우 호스트 메모리를 업데이트한다.
+	 * @param reset 변경 여부 플래그를 리셋할지 여부
+	 */
 	void checkDeviceMemAndUpdateHostMem(bool reset=true);
+	/**
+	 * @details 호스트 메모리의 변경 여부를 확인하고
+	 *          변경된 경우 디바이스 메모리를 업데이트한다.
+	 * @param reset 변경 여부 플래그를 리셋할지 여부
+	 */
 	void checkHostMemAndUpdateDeviceMem(bool reset=true);
 
+	/**
+	 * @details shape가 결정되어 메모리가 할당되었는지 확인
+	 */
 	void checkMemValidity();
 
 
 
 private:
-	size_t _size;
+	size_t _size;							///< 할당된 메모리의 크기
 
-	Dtype* _host_mem;
-	Dtype* _device_mem;
+	Dtype* _host_mem;						///< 호스트 메모리 포인터
+	Dtype* _device_mem;						///< 디바이스 메모리 포인터
 
-	bool _host_mem_updated;
-	bool _device_mem_updated;
+	bool _host_mem_updated;					///< 호스트 메모리 변경 여부 플래그
+	bool _device_mem_updated;				///< 디바이스 메모리 변경 여부 플래그
 
 };
 
