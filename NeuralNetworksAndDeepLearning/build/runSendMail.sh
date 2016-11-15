2093eaa097da910fd7e645be6e82af0bb4ed125b
build <build@grandsys-centos.(none)>
2016-11-15 12:54:30 +0900

    (1) runSendMail.sh를 빌드서버 환경에 맞게 수정

--- NeuralNetworksAndDeepLearning/build/runSendMail.sh
+++ NeuralNetworksAndDeepLearning/build/runSendMail.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+source /opt/rh/devtoolset-3/enable
 export PATH=/usr/local/cuda-8.0/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH
-source /home/monhoney/git/neuralnetworksanddeeplearning/NeuralNetworksAndDeepLearning/env.sh
-/home/monhoney/git/neuralnetworksanddeeplearning/NeuralNetworksAndDeepLearning/build/sendmail.py
+source /home/build/neuralnetworksanddeeplearning/NeuralNetworksAndDeepLearning/env.sh
+/home/build/neuralnetworksanddeeplearning/NeuralNetworksAndDeepLearning/build/sendmail.py
