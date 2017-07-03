f3fa1d0332f238b98ae80de5d22f5778cb99d798
build <build@grandsys-centos.(none)>
2017-07-03 17:16:00 +0900

    Fixed to make daily mail work

--- SoooA/build/runSendMail.sh
+++ SoooA/build/runSendMail.sh
@@ -3,6 +3,6 @@
 source /opt/rh/devtoolset-3/enable
 export PATH=/usr/local/cuda-8.0/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH
-source /home/build/neuralnetworksanddeeplearning/NeuralNetworksAndDeepLearning/env.sh
+source /home/build/soooa/SoooA/env.sh
 unset SSH_ASKPASS
-/home/build/neuralnetworksanddeeplearning/NeuralNetworksAndDeepLearning/build/sendmail.py
+/home/build/soooa/SoooA/build/sendmail.py
