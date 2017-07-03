fc521fe549e64e6ecb8c6402e3a5a671e76b5efc
build <build@grandsys-centos.(none)>
2017-07-03 17:20:48 +0900

    remove build server mail related files

--- SoooA/build/runSendMail.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source /opt/rh/devtoolset-3/enable
-export PATH=/usr/local/cuda-8.0/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH
-source /home/build/soooa/SoooA/env.sh
-unset SSH_ASKPASS
-/home/build/soooa/SoooA/build/sendmail.py
